dc2fd4ca23eeeb8a64bc20ee1cca4793d441dcda
Wayne J. Ryan <wryan@smithdrug.com>
2022-10-04 08:01:23 -0400

    Adding files to the initial framework

---
+++ SpringBootBackend/MyMoviePlan/src/test/java/com/example/mymovieplan/MyMoviePlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymovieplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviePlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
